41b9ee512da37a5cc0040b1a2a784622fb45c133
ignacio.grigas <ignacio.grigas@gmail.com>
2021-06-27 12:40:18 -0300

    Products pages

--- Corenergy/src/routes/web.js
+++ Corenergy/src/routes/web.js
@@ -10,6 +10,10 @@ app.get("/log_in",(req, res) => res.sendFile(path.resolve(__dirname,"../views/lo
 app.get("/profile",(req, res) => res.sendFile(path.resolve(__dirname,"../views/profile.html")))
 app.get("/shopping_cart",(req, res) => res.sendFile(path.resolve(__dirname,"../views/shopping_cart.html")))
 app.get("/sing_up",(req, res) => res.sendFile(path.resolve(__dirname,"../views/sing_up.html")))
+app.get("/Weights",(req, res) => res.sendFile(path.resolve(__dirname,"../views/Weights.html")))
+app.get("/Elastic_Bands",(req, res) => res.sendFile(path.resolve(__dirname,"../views/Elastic_Bands.html")))
+app.get("/Mats",(req, res) => res.sendFile(path.resolve(__dirname,"../views/Mats.html")))
+app.get("/Bars",(req, res) => res.sendFile(path.resolve(__dirname,"../views/Bars.html")))
 
 
 module.exports = app;
