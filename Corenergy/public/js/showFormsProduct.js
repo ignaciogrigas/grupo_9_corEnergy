0f45aad73b11cea22a45ae9fe92ab299843b1d60
ignacio.grigas <ignacio.grigas@gmail.com>
2021-09-29 09:33:05 -0300

    few js changes

---
+++ Corenergy/public/js/showFormsProduct.js
@@ -0,0 +1,9 @@
+let formToShow2 = document.querySelector("#form_to_show2")
+let plus2 = document.querySelector("#show_form2")
+plus2.addEventListener("click",function() {
+    if(formToShow2.style.display = "none"){
+        formToShow2.style.display = "flex"
+    }else {
+        formToShow2.style.display="none"
+    }
+})
